c5bfa7c3d7e262b9acd8cdbfe1e71299ee8f07a1
Your Name <mikachupichu@gmail.com>
2023-11-27 14:52:20 +0300

    Completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+    def match(self,word_list):
+        return [w for w in  word_list if self.compare_words(w)]
+
+    def compare_words(self,one_word):
+        return sorted(self.word.lower()) == sorted (one_word.lower())
